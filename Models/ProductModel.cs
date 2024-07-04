38f909e47e4efdd5ad354e2ec9f9db7b5d25b68a
Fabricio Melo <fabriciosartin4@hotmail.com>
2024-07-04 17:14:29 +0100

    Initial commit, building the foundation elements for further iterations

---
+++ Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualVendingMachine.Models
+{
+    public class ProductModel
+    {
+        public double UniqueID { get; set; } 
+        
+        public ProductShape ProductShape { get; set; }
+
+    }
+}
---
+++ ViewModels/SlotsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace VirtualVendingMachine.ViewModels
+{
+    public class SlotsViewModel
+    {
+        
+    }
+}
